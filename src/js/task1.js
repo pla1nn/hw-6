9c685d78ac0ac0b11aceece7381add367639f085
pla1n <danilkuisis@gmail.com>
2024-08-20 19:51:47 +0300

    as

--- src/js/task1.js
+++ src/js/task1.js
@@ -7,11 +7,14 @@ form.addEventListener('submit', submitForm);
 function submitForm(e) {
     e.preventDefault();
 
-    formData.name = document.getElementById('name').value;
-    formData.email = document.getElementById('email').value;
-    formData.message = document.getElementById('message').value;
-    
+    const element = e.target;
 
+    formData.name = element.name.value;
+    formData.email = element.email.value;
+    formData.message = element.message.value;
+    
+    console.log('form data', formData);
+    
     const success = document.getElementById('success');
 
     success.style.display = 'block';
